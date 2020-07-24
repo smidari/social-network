2130b5aab8cff9dd8fedf778ca835d5a9b4714ed
Dasha <smidariaa@gmail.com>
2020-07-24 18:15:36 +0300

    Changed style Navbar component

--- src/components/MyNavbar/MyNavbar.tsx
+++ src/components/MyNavbar/MyNavbar.tsx
@@ -4,21 +4,21 @@ import {NavLink} from "react-router-dom";
 
 const MyNavbar = () => (
     <>
-        <nav className={n.nav}>
+        <nav className={n.nav} >
             <div className={n.item}>
-                <NavLink to='/profile' activeClassName={n.active}>Profile</NavLink>
+                <NavLink to='/profile' activeClassName={n.active} >Profile</NavLink>
             </div>
             <div className={n.item}>
-                <NavLink to='/dialogs' activeClassName={n.active}>Messages</NavLink>
+                <NavLink to='/dialogs' activeClassName={n.active} >Messages</NavLink>
             </div>
             <div className={n.item}>
-                <NavLink to='/news' activeClassName={n.active}>News</NavLink>
+                <NavLink to='/news' activeClassName={n.active} >News</NavLink>
             </div>
             <div className={n.item}>
-                <NavLink to='/music' activeClassName={n.active}>Music</NavLink>
+                <NavLink to='/music' activeClassName={n.active} >Music</NavLink>
             </div>
             <div className={n.item}>
-                <NavLink to='/settings' activeClassName={n.active}>Settings</NavLink>
+                <NavLink to='/settings' activeClassName={n.active} >Settings</NavLink>
             </div>
         </nav>
     </>
