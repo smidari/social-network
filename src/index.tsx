3cccfaa8a2cd28c33a2f3243244fee7c222a9931
Dasha <smidariaa@gmail.com>
2020-08-03 13:25:28 +0300

    Create store

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import state, {RootStateType, subscribe, updateNewPostText} from "./redux/state";
-import {addPost} from "./redux/state";
+import store, {RootStateType} from "./redux/state";
 
 
  let rerenderEntireTree = (state: RootStateType) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App
+                state={state}
+                addPost={store.addPost.bind(store)}
+                updateNewPostText={store.updateNewPostText.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
 };
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -13,7 +13,7 @@ export type MessageType = {
 };
 export type ProfilePageType = {
     posts: Array<PostType>
-    newPostText:string
+    newPostText: string
 };
 export type MessagesPageType = {
     messages: Array<MessageType>
@@ -24,56 +24,67 @@ export type RootStateType = {
     messagesPage: MessagesPageType
 };
 
+export type StoreType = {
+    _state: RootStateType
+    getState: () => RootStateType
+    _callSubscriber: (state: RootStateType) => void
+    addPost: () => void
+    updateNewPostText: (newText: string) => void
+    subscribe: (observer: any) => void
 
-let rerenderEntireTree =(state: RootStateType) => {
+}
 
-};
-
-let state: RootStateType = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'How are you', likeCount: 10},
-            {id: 2, message: 'its my first posts', likeCount: 23},
-            {id: 3, message: 'hi', likeCount: 3},
-            {id: 4, message: 'its my first postsfsfsdfsdfsdfs', likeCount: 5},
-        ],
-        newPostText: 'it camasutra'
+let store: StoreType = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'How are you', likeCount: 10},
+                {id: 2, message: 'its my first posts', likeCount: 23},
+                {id: 3, message: 'hi', likeCount: 3},
+                {id: 4, message: 'its my first postsfsfsdfsdfsdfs', likeCount: 5},
+            ],
+            newPostText: 'it camasutra'
+        },
+        messagesPage: {
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'Lorem ipsum dolor sit amet, consectetur'},
+                {id: 3, message: 'Lorem ipsum dolor sit'},
+                {id: 4, message: 'Lorem ipsum dolor sit, Lorem ipsum dolor sit, Lorem ipsum dolor sit'}
+            ],
+            dialogs: [
+                {id: 1, name: 'Dima'},
+                {id: 2, name: 'Masha'},
+                {id: 3, name: 'Sasha'},
+                {id: 4, name: 'Dasha'},
+                {id: 5, name: 'Svetlana'},
+                {id: 6, name: 'Valera'}
+            ],
+        },
     },
-    messagesPage: {
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'Lorem ipsum dolor sit amet, consectetur'},
-            {id: 3, message: 'Lorem ipsum dolor sit'},
-            {id: 4, message: 'Lorem ipsum dolor sit, Lorem ipsum dolor sit, Lorem ipsum dolor sit'}
-        ],
-        dialogs: [
-            {id: 1, name: 'Dima'},
-            {id: 2, name: 'Masha'},
-            {id: 3, name: 'Sasha'},
-            {id: 4, name: 'Dasha'},
-            {id: 5, name: 'Svetlana'},
-            {id: 6, name: 'Valera'}
-        ],
+    getState (){return this._state},
+    _callSubscriber(state) {
+    },
+    addPost() {
+        const newPost: PostType = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state)
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     },
 };
 
-export const addPost = () => {
-    const newPost: PostType = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state)
-};
 
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state)
-};
-export const subscribe = (observer:any) => {
-    rerenderEntireTree = observer;
-};
-export default state;
+export default store;
 
+// window.store = store;
